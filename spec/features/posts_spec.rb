3161c7998df2f28478152cd1e7ba79eda298c61e
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-26 22:34:06 +0000

    Automatically backed up by Learn

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 describe 'Posts', type: 'feature' do
 
-  
+  describe 'new' do
+
+    before :each do
+      @cool = Category.create(name: 'Cool')
+      visit 'posts/new'
+      fill_in('post_title', :with => 'Feeling Awesome')
+      fill_in('post_content', :with => 'This is going to be an awesome post!')
+    end
 
     it 'can create a post without a new category' do
       click_button('Create Post')
